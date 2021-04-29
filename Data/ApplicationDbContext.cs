996ddfefe94fa300efa30d97af111f5cf5aceda9
Niko D <nikodojan@live.de>
2021-04-29 11:06:13 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using UserManagementTestApp.Models;
+
+namespace UserManagementTestApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ZpoolUser, IdentityRole<int>, int>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        
+    }
+}
---
+++ Models/ZpoolUser.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace UserManagementTestApp.Models
+{
+    public class ZpoolUser : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsDriver { get; set; }
+        public string? Gender { get; set; }
+        public string? Introduction { get; set; }
+        
+    }
+}
