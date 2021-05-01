a349cbbca309a0df0e5ebff3c9339b3f13dd8b15
Niko D <nikodojan@live.de>
2021-05-01 10:06:00 +0200

    -

---
+++ Pages/TestFolder/TestUserData.cshtml.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using UserManagementTestApp.Areas.Identity.Pages.Account.Manage;
+using UserManagementTestApp.Models;
+using ZPool.Models;
+
+namespace ZPool.Pages.TestFolder
+{
+    public class TestUserDataModel : PageModel
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private AppDbContext _context;
+
+        public TestUserDataModel(
+            UserManager<AppUser> userManager,
+            AppDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+        
+        public AppUser AppUser { get; set; }
+
+
+        
+        public IEnumerable<TestCar> Cars { get; set; } = new List<TestCar>();
+
+
+        public async Task<IActionResult> OnGet()
+        {
+            //AppUser = await _userManager.GetUserAsync(User);
+            //if (AppUser == null)
+            //{
+            //    return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            //}
+
+            AppUser = _userManager.Users.FirstOrDefault(u => u.Id == 1);
+
+            
+            Cars = _context.Cars.Where(c => c.UserId == AppUser.Id);
+
+            return Page();
+        }
+    }
+}
