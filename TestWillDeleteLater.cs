7a5ab114b8ae8e3db9514a345bc30fc3415b5c3a
Nohely <ngedeon14@yahoo.com>
2021-05-03 09:19:48 +0200

    Just testing if everythig works.

---
+++ TestWillDeleteLater.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZPool
+{
+    public class TestWillDeleteLater
+    {
+        //This is a test 
+
+        //Going to delete later 
+    }
+}
