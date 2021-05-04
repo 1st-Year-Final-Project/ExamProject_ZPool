6d3facb8b443b15465b8c57d1ca1628f8cc8586e
JaNeviemMeno <75016040+JaNeviemMeno@users.noreply.github.com>
2021-05-04 16:53:05 +0200

    Updating stuff about Car.. CreatePage, Interface, EFService...

--- Models/Car.cs
+++ Models/Car.cs
@@ -8,8 +8,10 @@ namespace ZPool.Models
     public class Car
     {
         public int CarID { get; set; }
-        public string Make { get; set; }
-        public string Colour { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string NumberPlate { get; set; }
+        public string Color { get; set; }
         public int NumberOfSeats { get; set; }
 
 
