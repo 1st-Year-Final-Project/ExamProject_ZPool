fc39e416a3ce84d5f00dfb12c95a2225441a1f50
Niko D <nikodojan@live.de>
2021-05-04 10:48:30 +0200

    email

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -15,7 +15,6 @@ namespace UserManagementTestApp.Models
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
         public string? Gender { get; set; }
         public string? Introduction { get; set; }
 
