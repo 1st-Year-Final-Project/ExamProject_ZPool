19730b2f01a562f417e1737b7c03e7af5257200a
JaNeviemMeno <75016040+JaNeviemMeno@users.noreply.github.com>
2021-05-05 20:12:52 +0200

    Small error in the name of the folder for interfaces is corrected

--- Pages/Rides/EditRide.cshtml.cs
+++ Pages/Rides/EditRide.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ZPool.Models;
-using ZPool.Services.Interfaces;
+using ZPool.Services.Interface;
 
 namespace ZPool.Pages.Rides
 {
