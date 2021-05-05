f8479061ff732769b2d82367e0e9885405223d9c
wenqi huang <wenqi100@gmail.com>
2021-05-05 21:55:07 +0200

    profile

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -18,8 +18,12 @@ namespace UserManagementTestApp.Models
         public override int Id { get; set; } // this is overridden from the baseclass, pls don't change
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string? Gender { get; set; }
+        public string? Gender { get; set; }  /// suggest: Gender should NOT be optional, because it doesn't need to be changed.
         public string? Introduction { get; set; }
+
+        /// suggest: add an attribute of profilePicture
+
+        /// Username should be the whole email address, but currently it is just the part before @. 
 
     }
 }
