e2da59fb31e9eafb154f6a8a42eec6dc6c0e016a
Niko D <nikodojan@live.de>
2021-05-21 12:21:04 +0200

    images folder

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -11,11 +11,10 @@ namespace UserManagementTestApp.Models
 {
     public class AppUser : IdentityUser<int>
     {
-        // The user Id is part of the base class IdentityUser and is named: Id
-        // Email and others are also members of the base class
+        // Email and other props are members of the base class
         // and do not require a concrete implementation
 
-        public override int Id { get; set; } // this is overridden from the baseclass, pls don't change
+        public override int Id { get; set; } 
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string? Gender { get; set; }
