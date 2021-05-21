b1c1dcaa557e69d8c9946f831cb9fa66a71eaf5f
Niko D <nikodojan@live.de>
2021-05-21 15:38:29 +0200

    .

--- Startup.cs
+++ Startup.cs
@@ -43,7 +43,7 @@ namespace ZPool
             
             services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("CloudConnection")));
+                    Configuration.GetConnectionString("LocalConnection")));
 
             services.AddIdentity<AppUser, IdentityRole<int>>(options =>
             {
