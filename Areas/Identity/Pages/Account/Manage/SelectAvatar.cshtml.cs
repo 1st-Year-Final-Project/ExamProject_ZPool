cb5743426c7cab657fdce9c7ad519cf2ff53deab
JaNeviemMeno <75016040+JaNeviemMeno@users.noreply.github.com>
2021-05-21 11:57:09 +0200

    small change

--- Areas/Identity/Pages/Account/Manage/SelectAvatar.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/SelectAvatar.cshtml.cs
@@ -71,7 +71,7 @@ namespace ZPool.Areas.Identity.Pages.Account.Manage
             }
 
             
-            return RedirectToPage();
+            return RedirectToPage("Index");
         }
 
     }
